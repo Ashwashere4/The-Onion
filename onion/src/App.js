832ebb86831c4684653e89a86cca3345f851f71d
Cadenmf24 <cadenmf24@gmail.com>
2024-06-15 20:19:39 -0400

    list of titles

--- onion/src/App.js
+++ onion/src/App.js
@@ -2,18 +2,43 @@ import logo from './logo.png';
 import './App.css';
 import SearchBar from './Components/SearchBar';
 import React from 'react'
+import listOfTitles from './titles.txt?raw';
 
 class App extends React.Component{
+
+  listOfTitles = [
+    "What are we making today?",
+    "Feeling Saucy?",
+    "Chat! Let. Them. COOK!",
+    "Breakfast, Lunch, Dinner! We got em all!",
+    "Midnight snack? Na, just an early breakfast",
+    "Breakfast Dinner! Dinner Breakfast!",
+    "[Insert Text Here]",
+    "Not to be confused with the journal..."
+  ];
+
+  changeTitle = () =>{
+
+
+    let int = Math.floor(Math.random() * this.listOfTitles.length)
+
+    this.setState({title: this.listOfTitles[int]})
+    
+    // generate a random number, find that sentence, then return the sentance
+  }
+
 
   constructor(props){
     super(props);
     this.state = {
-      database : []
+      database : [],
+      title : "Damn, that's crazy"
     }
   }
 
   componentDidMount(){
     this.updateDatabase()
+    this.changeTitle()
   }
 
   updateDatabase = () =>{
@@ -35,7 +60,7 @@ class App extends React.Component{
           {/* Get a new logo because copyright bullshit */}
           <img src={logo} className="App-logo" alt="logo" /> 
           <p>
-            What's cooking good looking
+            {this.state.title}
           </p>
 
           <SearchBar placeholder={"Let's get cooking..."} data = {this.state.database}/>
--- onion/src/Components/SearchBar.js
+++ onion/src/Components/SearchBar.js
@@ -1,23 +1,42 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './SearchBar.css'
 import SearchIcon from '@mui/icons-material/Search';
 
 function SearchBar({placeholder, data}){
+
+    const [filteredData, setFilteredData] = useState([]);
+
+    const handleFilter = (event) => {
+        const searchWord = event.target.value
+        const newFilter = data.filter((value) => {
+            return value.recipe.toLowerCase().includes(searchWord.toLowerCase());
+        });
+
+        if(searchWord === "" ){
+            setFilteredData([])
+        } else{
+
+        setFilteredData(newFilter);
+    }
+};
     return (
         <div className="search">
             <div className = "searchInputs">
-                <input type= "text" placeholder={placeholder}/> 
+                <input type= "text" placeholder={placeholder} onChange={handleFilter}/> 
                 <div className = "searchIcon">
                     <SearchIcon color = "secondary"/> 
                 </div>
             </div>
+            {filteredData.length !== 0 && (
             <div className = "dataResult">
-                {data.map((value, key) => {
-                    return <a className = "dataItem"> {value.recipe} </a>
+                {filteredData.map((value, key) => {
+                    return <p className = "dataItem"> {value.recipe} </p>
                 })}
             </div>
+            )}
         </div>
     )
 }
+
 
 export default SearchBar

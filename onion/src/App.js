63f15f38a08347030e0045553d687f0e7c0415f0
Cadenmf24 <cadenmf24@gmail.com>
2024-07-29 16:15:07 -0400

    stack overflow told me an ip to try

--- onion/src/App.js
+++ onion/src/App.js
@@ -5,7 +5,7 @@ import React from 'react'
 import AdminModal from './Components/AdminModal';
 // import Button from '@mui/material/Button';
 
-const ip = "127.0.0.1"
+const ip = "172.17.0.1"
 
 
 class App extends React.Component{
